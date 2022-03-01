e2d8e8d8d565ab35209abba7d33b536c636e438a
Alexy <britvinaps@gmail.com>
2022-03-01 22:36:56 +0300

    2

--- src/components/calculator/Calculator.vue
+++ src/components/calculator/Calculator.vue
@@ -183,10 +183,12 @@ h2 {
 }
 .calculator {
   min-width: 200px;
-  position: absolute;
+  max-width: 600px;
+  margin: 0 auto;
+  /* position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   padding: 10px;
 }
 .form {
